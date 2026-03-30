e48ac146e889c03c21d35cac2fe5cef34bcd4b56
20016375 <alouacehyannis1@gmail.com>
2026-03-30 16:17:04 +0200

    feat: add link component to design system
    Created ds-link component with 3 variants:
    - simple: black text, coral on hover with underline
    - button: filled coral button-style link
    - underlined: coral color with permanent underline
    
    All variants support small, medium, and large sizes.
    Added showcase section to design system page.

--- pawCare/src/app/design-system/design-system.module.ts
+++ pawCare/src/app/design-system/design-system.module.ts
@@ -4,8 +4,10 @@ import { ButtonComponent } from './button/button.component';
 import { LightboxComponent } from './lightbox/lightbox.component';
 import { AvatarComponent } from './avatar/avatar.component';
 import { CardComponent } from './card/card.component';
+import { LinkComponent } from './link/link.component';
+
 @NgModule({
-  imports: [TitleComponent, ButtonComponent, LightboxComponent, AvatarComponent, CardComponent],
-  exports: [TitleComponent, ButtonComponent, LightboxComponent, AvatarComponent, CardComponent]
+  imports: [TitleComponent, ButtonComponent, LightboxComponent, AvatarComponent, CardComponent, LinkComponent],
+  exports: [TitleComponent, ButtonComponent, LightboxComponent, AvatarComponent, CardComponent, LinkComponent]
 })
 export class DesignSystemModule {}
