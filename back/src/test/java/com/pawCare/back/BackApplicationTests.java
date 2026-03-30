c8f5133559e1cdb4f97996246cc9d0ff0f7b10ac
Elsaay <youslisa07@gmail.com>
2026-03-30 18:35:05 +0200

    Feat : init back with java spring boot, and rename the folder pawCare to front

---
+++ back/src/test/java/com/pawCare/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawCare.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
