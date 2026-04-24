30e702152f73ede7b70a77b75946f0d930f4c780
20016375 <alouacehyannis1@gmail.com>
2026-04-24 21:46:45 +0200

    feat: add animal gallery page with styling and functionality

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DesignSystemPageComponent } from './pages/design-system-page/design-system-page.component';
 import { AnimalProfileSelectorPageComponent } from './pages/animal-profile-selector-page/animal-profile-selector-page.component';
+import { AnimalGalleryPageComponent } from './pages/animal-gallery-page/animal-gallery-page.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'select-profile',
     component: AnimalProfileSelectorPageComponent
+  },
+  {
+    path: 'animals/gallery',
+    component: AnimalGalleryPageComponent
   },
   {
     path: 'design-system',
