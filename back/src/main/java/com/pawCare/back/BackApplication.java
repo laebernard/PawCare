5c906948bca21df62feaad9c5b3d25083a9e620c
Elsaay <youslisa07@gmail.com>
2026-05-20 21:09:46 +0200

    feat: add dotenv support for environment variables and update JWT configuration

--- back/src/main/java/com/pawCare/back/BackApplication.java
+++ back/src/main/java/com/pawCare/back/BackApplication.java
@@ -1,5 +1,6 @@
 package com.pawCare.back;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(BackApplication.class, args);
 	}
 
